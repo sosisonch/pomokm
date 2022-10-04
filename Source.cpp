c50dbe91500178aa549c8b5d180a3c30b5eeee28
sosisonch <114304489+sosisonch@users.noreply.github.com>
2022-10-04 18:41:54 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	/*
+	Case1.Дано целое число в диапазоне 1–7.Вывести строку — название дня
+		недели, соответствующее данному числу(1 — «понедельник», 2 — «вторник» и т.д.).*/
+	/*int day;
+	cin >> day;
+	if (day == 1) {
+		cout << "понедельник"
+	}
+	else if (day == 2) {
+		cout << "вторник" << endl;
+	}
+	else if (day == 7); {
+		cout << "воскресенье"
+	}
+	else {
+		cout << "ошибочный номер дня" << endl
+	}*/
+	/*	int day;
+		cin >> day;
+		switch (day) {
+		case 1: cout << "понедельник" << endl; break;
+		case 2: cout << "вторник" << endl;break;
+		case 3: cout << "среда" << endl; break;
+		case 4: cout << "четверг" << endl; break;
+		case 5: cout << "пятница" << endl; break;
+		case 6: cout << "суббота" << endl; break;
+		case 7: cout << "воскресенье" << endl; break;
+		default: cout << "ошибочный номер дня" << endl;
+		}*/
+	
+	//Case2.Дано целое число K.Вывести строку - описание оценки,
+		//соответствующей числу K(1 — «плохо», 2 — «неудовлетворительно», 3 — «удовлетворительно», 4 — «хорошо», 5 — «отлично»).
+		//Если K не лежит в диапазоне 1–5,
+	//	то вывести строку «ошибка».
+	/*int k;
+	cin >> k;
+	switch (k); {
+	case 1: cout << "плохо" << endl; break;
+	case 2: cout << "неудовлетворительно" << endl; break;
+	case 3: cout << "удовлетворительно" << endl; break;
+	case 4: cout << "хорошо" << endl; break;
+	case 5: cout << "отлично" << endl; break;
+	default: cout << "ошибка" endl; break;
+	}*/
+//Case4◦. Дан номер месяца — целое число в диапазоне 1–12
+	//	(1 — январь, 2 — февраль и т.д.).Определить количество дней в этом месяце для невисокосного года.
+		/*int month;
+		cin >> month;
+		switch (month) {
+		case 1:cout << "январь" << endl; break;
+		case 2:cout << "февраль" << endl; break;
+		case 3:cout << "март" << endl; break;
+		case 4:cout << "апрель" << endl; break;
+		case 5:cout << "май" << endl; break;
+		case 6:cout << "июнь" << endl; break;
+		case 7:cout << "июль" << endl; break;
+		case 8:cout << "август" << endl; break;
+		case 9:cout << "сентябрь" << endl; break;
+		case 10:cout << "октябрь" << endl; break;
+		case 11:cout << "ноябрь" << endl; break;
+		case 12:cout << "декабрь" << endl; break;
+		case 2:
+		}*/
+	//1
+/*int a;
+ cin >> a;
+ switch (a) {
+ case 1:cout << "верно " << endl;  break;
+ default: cout << "неверно" << endl; break;
+ }*/
+	//2
+ /*int a;
+ cin >> a;
+ if (a > 0) {
+	 cout << "верно" << endl;
+ }
+ else (a > 0); "неверно" << endl;
+ }*/
+	//4
+	/*int a;
+	cin >> a;
+	if (a >= 0) {
+	cout << "верно";
+	}
+	else {
+	cout << "неверно";
+	*/
+	//5
+	/*int a;
+	cin >> a;
+	if (a <= 0) {
+	cout << "верно"
+	}
+	else ( a >= 0);
+	cout << "неверно";
+	*/
+	 return 0;
+}
